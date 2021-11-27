0f5398b223bc733acac0b65f2b47dcbad5644b18
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-27 00:12:33 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function focused(){
+        console.log('Good!')
+    }
+    function blured(){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+    <button onFocus={focused} onBlur={blured}>Eyes on me</button>)
+}
+
+export default EyesOnMe;
